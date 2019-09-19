0495e3f7a72e0fc42016964865ecc9d3129b9d97
Weidong Zhang <william_zwd@outlook.com>
2019-09-18 22:29:56 -0400

    .Net Core auction project

---
+++ Auction/Hubs/AuctionHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Auction.Hubs
+{
+    public class AuctionHub: Hub
+    {
+        public async Task SendBidding(string user, decimal price)
+        {
+            await Clients.All.SendAsync("ReceiveBidding", user, price);
+        }
+    }
+}
---
+++ Auction/wwwroot/js/auction.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/auctionHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveBidding", function (user, price) {
+    var currentdate = new Date(); 
+    var datetime = currentdate.getFullYear() + "-"
+        + (currentdate.getMonth() + 1) + "-" 
+        + currentdate.getDate() + " "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
+    var row = "<tr><td>" + user + "</td><td>$" + parseFloat(price, 10).toFixed(2) + "</td><td>" + datetime + "</td></tr>";
+    $('#auctionHistory tr:last').after(row);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var price = document.getElementById("priceInput").value;
+    connection.invoke("SendBidding", user, price).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
